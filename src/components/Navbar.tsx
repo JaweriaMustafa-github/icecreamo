7d7d4c66dc89c252cb773a86b70938e23ea607fc
JaweriaMustafa-github <jaweriamustafa01nov@gmail.com>
2024-11-12 13:32:04 +0500

    Created Icecreamo Website

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,5 @@
 import React from "react";
 import Link from "next/link"
-import { LiaIceCreamSolid } from "react-icons/lia";
-import { FaSearch} from "react-icons/fa";
-import { AiFillThunderbolt } from "react-icons/ai";
-import { FaShoppingCart } from "react-icons/fa";
-
-interface CustomIconProps
-  extends React.ComponentProps<typeof LiaIceCreamSolid> {
-  className?: string;
-}
-const CustomLiaIceCreamSolid: React.FC<CustomIconProps> = (props) => (
-  <LiaIceCreamSolid {...props} />
-);
-
-interface CustomIconProps extends React.ComponentProps<typeof FaSearch> {
-  className?: string;
-}
-const CustomFaSearch: React.FC<CustomIconProps> = (props) => (
-  <FaSearch {...props} />
-);
-
-interface CustomIconProps extends React.ComponentProps<typeof AiFillThunderbolt> {
-  className?: string;
-}
-const CustomAiFillThunderbolt: React.FC<CustomIconProps> = (props) => (
-  <AiFillThunderbolt {...props} />
-);
-
-interface CustomIconProps extends React.ComponentProps<typeof FaShoppingCart> {
-  className?: string;
-}
-const CustomFaCartShopping: React.FC<CustomIconProps> = (props) => (
-  <FaShoppingCart {...props} />
-);
 
 export default function Navbar() {
   return (
@@ -46,7 +13,6 @@ export default function Navbar() {
               </div>
 
               <div className="flex justify-center items-center gap-2">
-                <CustomAiFillThunderbolt className="justify-items-center w-5 h-5 text-purple-800 hidden lg:block md:block" />
                 <p className="text-sm text-white hidden lg:block md:block ">
                   Order now and get it within
                   <span className="text-pink-800"> 30 Minutes!</span>
